eb8b79a05f5c915724ba030d422727a7fb3a5765
normyee <lunoqro@gmail.com>
2023-10-14 12:30:11 -0300

    build

---
+++ televolve-ui/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+  <form>
+    <input type="text" placeholder="Email">
+    <input type="text" placeholder="Senha">
+  </form>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ televolve-ui/src/views/RegisterView.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>Register</h1>
+<form>
+    <input type="text" placeholder="Email">
+    <input type="text" placeholder="Senha">
+    <input type="text" placeholder="Repita sua senha">
+  </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
