29f848a880f34fbf36b1dffc858e3101d517394d
normyee <lunoqro@gmail.com>
2023-10-14 12:33:55 -0300

    feat: add firebase service

---
+++ televolve-ui/src/services/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAQluLLC3MgVyWHqWfLutPWGLl7N-B1rLg',
+  authDomain: 'vue-fire-auth-730db.firebaseapp.com',
+  projectId: 'vue-fire-auth-730db',
+  storageBucket: 'vue-fire-auth-730db.appspot.com',
+  messagingSenderId: '795709458956',
+  appId: '1:795709458956:web:79df97701d106badb47c29'
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
