4294291e7182901874ab3e5a912ee329853e702e
normyee <lunoqro@gmail.com>
2023-10-15 19:12:33 -0300

    fix: match the logout method with store

--- televolve-ui/src/views/HomeView.vue
+++ televolve-ui/src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
   <main class="home">
     <h1>Home</h1>
-    <button @click="$store.dispatch('logout')">Sair</button>
+    <button @click="$store.dispatch('logOut')">Sair</button>
   </main>
 </template>
 
 <script setup>
 import { useStore } from 'vuex';
 
+// eslint-disable-next-line no-unused-vars
 const store = useStore();
 </script>
 
