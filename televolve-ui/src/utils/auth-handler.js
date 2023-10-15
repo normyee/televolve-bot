6b3171abc15a871a518393fd9a93e013b8531ba5
normyee <lunoqro@gmail.com>
2023-10-15 13:59:32 -0300

    chore: changed helper.js to auth-handler.js

---
+++ televolve-ui/src/utils/auth-handler.js
@@ -0,0 +1,30 @@
+export class HandleAuthError {
+  constructor() {}
+
+  execute(error) {
+    switch (error.code) {
+      case 'auth/user-not-found':
+        alert('User not found');
+        break;
+      case 'auth/wrong-password':
+        alert('Wrong password');
+        break;
+      case 'auth/email-already-in-use':
+        alert('Email already in use');
+        break;
+      case 'auth/invalid-email':
+        alert('Invalid email');
+        break;
+      case 'auth/operation-not-allowed':
+        alert('Operation not allowed');
+        break;
+      case 'auth/weak-password':
+        alert('Weak password');
+        break;
+      default:
+        alert('Something went wrong');
+    }
+  }
+}
+
+export default new HandleAuthError()
