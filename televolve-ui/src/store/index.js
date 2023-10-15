9b3aad25826da65aac32a7e8d92277e579deacfc
normyee <lunoqro@gmail.com>
2023-10-15 19:12:00 -0300

    refactor: better readability and cleaner code

--- televolve-ui/src/store/index.js
+++ televolve-ui/src/store/index.js
@@ -1,12 +1,5 @@
 import { createStore } from 'vuex';
-import router from '../router';
-import { auth } from '../services/firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import handleAuthError from '../utils/auth-handler';
-
-const HOME_ROUTE = '/home';
-const LOGIN_ROUTE = '/login';
-const REGISTER_ROUTE = '/register';
+import authPerform from '../utils/auth-perform';
 
 export default createStore({
   state: {
@@ -20,46 +13,27 @@ export default createStore({
       state.user = null;
     }
   },
+
+  // Seção de métodos que lidam com lógica de autenticação.
   actions: {
+    // Função que loga.
     async login({ commit }, userDetails) {
-      const { email, password } = userDetails;
-
-      try {
-        await signInWithEmailAndPassword(auth, email, password);
-        commit('SET_USER', auth.currentUser);
-        router.push(HOME_ROUTE);
-      } catch (error) {
-        handleAuthError.execute(error);
-      }
+      await authPerform.signInUser(commit, userDetails);
     },
+
+    // Função que cadastra.
     async register({ commit }, userDetails) {
-      const { email, password } = userDetails;
-
-      try {
-        await createUserWithEmailAndPassword(auth, email, password);
-        commit('SET_USER', auth.currentUser);
-        router.push(HOME_ROUTE);
-      } catch (error) {
-        handleAuthError.execute(error);
-      }
+      await authPerform.signUpUser(commit, userDetails);
     },
-    async logout({ commit }) {
-      await signOut(auth);
-      commit('CLEAR_USER');
-      router.push(LOGIN_ROUTE);
-    },
-    fetchUser({ commit }) {
-      auth.onAuthStateChanged(async (user) => {
-        if (user === null) {
-          commit('CLEAR_USER');
-        } else {
-          commit('SET_USER', user);
 
-          if (router.isReady() && router.currentRoute.value.path === LOGIN_ROUTE || REGISTER_ROUTE) {
-            router.push(HOME_ROUTE);
-          }
-        }
-      });
+    // Função que desloga usuário.
+    async logOut({ commit }) {
+      await authPerform.logOut(commit);
+    },
+
+    // Função que lida o estado atual do usuário.
+    async fetchUser({ commit }) {
+      await authPerform.findUser(commit);
     }
   }
 });
