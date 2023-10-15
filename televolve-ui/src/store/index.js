91aed179ce8c29aef1960046a2e82f608126ffce
normyee <lunoqro@gmail.com>
2023-10-15 14:00:41 -0300

    refactor: improving readability

--- televolve-ui/src/store/index.js
+++ televolve-ui/src/store/index.js
@@ -1,8 +1,11 @@
-/* eslint-disable no-unused-vars */
 import { createStore } from 'vuex';
 import router from '../router';
 import { auth } from '../services/firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import handleAuthError from '../utils/auth-handler';
+
+const HOME_ROUTE = '/home';
+const LOGIN_ROUTE = '/login';
 
 export default createStore({
   state: {
@@ -17,62 +20,32 @@ export default createStore({
     }
   },
   actions: {
-    async login({ commit }, details) {
-      const { email, password } = details;
+    async login({ commit }, userDetails) {
+      const { email, password } = userDetails;
 
       try {
         await signInWithEmailAndPassword(auth, email, password);
+        commit('SET_USER', auth.currentUser);
+        router.push(HOME_ROUTE);
       } catch (error) {
-        switch (error.code) {
-          case 'auth/user-not-found':
-            alert('User not found');
-            break;
-          case 'auth/wrong-password':
-            alert('Wrong password');
-            break;
-          default:
-            alert('Something went wrong');
-        }
-        return;
+        handleAuthError.execute(error);
       }
-      commit('SET_USER', auth.currentUser);
-
-      router.push('/home');
     },
-    async register({ commit }, details) {
-      const { email, password } = details;
+    async register({ commit }, userDetails) {
+      const { email, password } = userDetails;
 
       try {
         await createUserWithEmailAndPassword(auth, email, password);
+        commit('SET_USER', auth.currentUser);
+        router.push(HOME_ROUTE);
       } catch (error) {
-        switch (error.code) {
-          case 'auth/email-already-in-use':
-            alert('Email already in use');
-            break;
-          case 'auth/invalid-email':
-            alert('Invalid email');
-            break;
-          case 'auth/operation-not-allowed':
-            alert('Operation not allowed');
-            break;
-          case 'auth/weak-password':
-            alert('Weak password');
-            break;
-          default:
-            alert('Something went wrong');
-        }
-        return;
+        handleAuthError.execute(error);
       }
-      commit('SET_USER', auth.currentUser);
-
-      router.push('/home');
     },
     async logout({ commit }) {
       await signOut(auth);
-
       commit('CLEAR_USER');
-
-      router.push('/login');
+      router.push(LOGIN_ROUTE);
     },
     fetchUser({ commit }) {
       auth.onAuthStateChanged(async (user) => {
@@ -81,8 +54,8 @@ export default createStore({
         } else {
           commit('SET_USER', user);
 
-          if (router.isReady() && router.currentRoute.value.path === '/login') {
-            router.push('/home');
+          if (router.isReady() && router.currentRoute.value.path === LOGIN_ROUTE) {
+            router.push(HOME_ROUTE);
           }
         }
       });
