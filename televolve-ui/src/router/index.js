8ca98338049a33d195ff5dedeb00f24b63f427a6
normyee <lunoqro@gmail.com>
2023-10-15 11:42:39 -0300

    feat: user cannot access home route if not logged in

--- televolve-ui/src/router/index.js
+++ televolve-ui/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import RegisterView from '../views/RegisterView.vue';
 import LoginView from '../views/LoginView.vue';
 import HomeView from '../views/HomeView.vue';
+import { auth } from '../services/firebase';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,6 +26,18 @@ const router = createRouter({
       }
     }
   ]
+});
+
+router.beforeEach((to, _from, next) => {
+  if (to.path === '/login' && auth.currentUser) {
+    next('/home');
+    return;
+  }
+  if (to.matched.some((record) => record.meta.requiresAuth) && !auth.currentUser) {
+    next('/login');
+    return;
+  }
+  next();
 });
 
 export default router;
