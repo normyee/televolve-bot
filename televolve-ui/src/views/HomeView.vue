396969abea11a66df08f38cc5041d5a8a8b5961f
normyee <lunoqro@gmail.com>
2023-10-15 12:50:09 -0300

    chore: add logout button and logout action

--- televolve-ui/src/views/HomeView.vue
+++ televolve-ui/src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
   <main class="home">
     <h1>Home</h1>
+    <button @click="$store.dispatch('logout')">Sair</button>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from 'vuex';
+
+const store = useStore();
+</script>
 
 <style lang="scss" scoped></style>
