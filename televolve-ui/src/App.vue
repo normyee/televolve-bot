1edf7a5f792e8ea545f99295b10e39eaa54134bc
normyee <lunoqro@gmail.com>
2023-10-15 12:47:16 -0300

    feat: logout button

--- televolve-ui/src/App.vue
+++ televolve-ui/src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import { onBeforeMount } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+//Used onBeforeMount to check if there's an user.
+onBeforeMount(() => {
+  store.dispatch('fetchUser');
+});
 </script>
 
 <template>
