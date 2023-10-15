e083e4214330eac63f7c6cda1d933de6c6c1d625
normyee <lunoqro@gmail.com>
2023-10-14 23:35:59 -0300

    chore: add routers and meta property

--- televolve-ui/src/router/index.js
+++ televolve-ui/src/router/index.js
@@ -1,24 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 import RegisterView from '../views/RegisterView.vue';
+import LoginView from '../views/LoginView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
       path: '/register',
       name: 'register',
       component: RegisterView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 });
