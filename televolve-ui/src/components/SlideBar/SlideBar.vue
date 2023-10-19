84fe195474149c00aee6a8eeba2fd02b30c490e5
normyee <lunoqro@gmail.com>
2023-10-19 17:29:46 -0300

    refactor

---
+++ televolve-ui/src/components/SlideBar/SlideBar.vue
@@ -0,0 +1,240 @@
+<template>
+  <div class="slidebar" @click="clickedChat($event)">
+    <div class="header">
+      <div class="avatar">
+        <img :src="botProfileImg" alt="profile" />
+      </div>
+    </div>
+    <div class="slidebar-chats">
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>SISIAMIMI</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+      <div class="slidebar-chat">
+        <div class="chat-avatar">
+          <img :src="defaultProfile" alt="profile-icon" />
+        </div>
+        <div class="chat-info">
+          <h4>USUÁRIO</h4>
+          <p>Última mensagem</p>
+        </div>
+        <div class="time">
+          <p>2:50pm</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import botProfileImg from '../../assets/img/profile.png';
+import defaultProfile from '../../assets/img/telegram.png';
+import { ref } from 'vue';
+
+// ref para selecionar o elemento anterior.
+const selectedChat = ref(null);
+
+const clickedChat = (e) => {
+  const target = e.target.closest('.slidebar-chat');
+  if (target) {
+    // Remove a cor de fundo do elemento anterior clicado.
+    if (selectedChat.value) {
+      selectedChat.value.style.backgroundColor = '';
+    }
+    // Define a cor de fundo no elemento clicado.
+    target.style.backgroundColor = '#c2c5c6';
+    // Armazena o elemento clicado como o novo elemento selecionado.
+    selectedChat.value = target;
+  }
+};
+</script>
+
+<style src="./SlideBar.css" scoped></style>
--- televolve-ui/src/views/HomeView.vue
+++ televolve-ui/src/views/HomeView.vue
@@ -1,244 +1,7 @@
 <template>
   <div class="container">
     <main class="centered-div">
-      <div class="slidebar" @click="clickedChat($event)">
-        <div class="header">
-          <div class="avatar">
-            <img src="../assets/img/profile.png" alt="profile" />
-          </div>
-        </div>
-        <div class="slidebar-chats">
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>SISIAMIMI</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-          <div class="slidebar-chat">
-            <div class="chat-avatar">
-              <img src="../assets/img/telegram.png" alt="profile-icon" />
-            </div>
-            <div class="chat-info">
-              <h4>USUÁRIO</h4>
-              <p>Última mensagem</p>
-            </div>
-            <div class="time">
-              <p>2:50pm</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      <SlideBar></SlideBar>
       <div class="message-container">
         <div class="header">
           <div class="chat-title">
@@ -270,8 +33,12 @@
           <p class="chat-message chat-sent bubble right">
             Mensagem ENVIADA 👻<span class="chat-time">11:39 pm</span>
           </p>
-          <p v-for="(message, index) in sentMessages" :key="index" class="chat-message chat-sent bubble right">
-           {{ message.text }}<span class="chat-time">{{message.time}}</span>
+          <p
+            v-for="(message, index) in sentMessages"
+            :key="index"
+            class="chat-message chat-sent bubble right"
+          >
+            {{ message.text }}<span class="chat-time">{{ message.time }}</span>
           </p>
         </div>
         <MessageFooter @send="send"></MessageFooter>
@@ -282,15 +49,13 @@
 
 <script setup>
 import MessageFooter from '../components/MessageFooter/MessageFooter.vue';
+import SlideBar from '../components/SlideBar/SlideBar.vue';
 import { useStore } from 'vuex';
 import { ref } from 'vue';
 
 // eslint-disable-next-line no-unused-vars
 const store = useStore();
-
-const selectedChat = ref(null);
 const sentMessages = ref([]);
-
 
 // Formatação de horas.
 const formatTime = () => {
@@ -306,21 +71,6 @@ const formatTime = () => {
 
   //Retorna formatado o horário.
   return `${formattedHours}:${formattedMinutes}`;
-}
-
-
-const clickedChat = (e) => {
-  const target = e.target.closest('.slidebar-chat');
-  if (target) {
-    // Remove a cor de fundo do elemento anterior clicado.
-    if (selectedChat.value) {
-      selectedChat.value.style.backgroundColor = '';
-    }
-    // Define a cor de fundo no elemento clicado.
-    target.style.backgroundColor = '#c2c5c6';
-    // Armazena o elemento clicado como o novo elemento selecionado.
-    selectedChat.value = target;
-  }
 };
 
 // Função que recebe o valor do input do elemento filho do componente MessageFooter.
@@ -328,17 +78,12 @@ const send = ({ messageInput }) => {
   const messageText = messageInput._value;
 
   if (messageText.trim() !== '') {
-
-
-
     sentMessages.value.push({
       text: messageText,
-      time: formatTime(),
+      time: formatTime()
     });
   }
-  
 };
-
 </script>
 
 <style src="./HomeView.css" scoped></style>
