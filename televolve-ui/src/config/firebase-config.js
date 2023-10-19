89028dcb55113409b6197cd9c8b42fffb44cecbb
normyee <lunoqro@gmail.com>
2023-10-19 16:24:32 -0300

    chore: firebase config file

---
+++ televolve-ui/src/config/firebase-config.js
@@ -0,0 +1,10 @@
+// Dados sensíveis da sua API do Firebase.
+
+export const firebaseConfig = {
+    apiKey: '####',
+    authDomain: '####',
+    projectId: '####',
+    storageBucket: '####',
+    messagingSenderId: '####',
+    appId: '####'
+}
